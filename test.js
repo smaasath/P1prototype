db3fc5dfef6d37c253aa00e60748062a088eaf1c
smaasath <99379342+smaasath@users.noreply.github.com>
2023-05-13 20:30:16 +0530

    Update test.js

--- test.js
+++ test.js
@@ -8,13 +8,29 @@ function handleSubmit(event) {
     var home = document.getElementById('homeinput').value;
     var featuters = document.getElementById('feainput').value;
     var price = document.getElementById('priinput').value;
+    var bold = document.getElementById('flexCheckDefault');
+    // document.getElementById('xxx').checked
     
     
     // Print the data in HTML
-    document.getElementById("output").innerHTML = name;
+    document.getElementById("output").innerText = name;
     document.getElementById("home").innerHTML =home;
     document.getElementById("Features").innerHTML = featuters;
     document.getElementById("Pricing").innerHTML = price;
+    
+    // const newDiv = document.querySelector('#output');
+
+    if(bold.checked){      
+      document.getElementById("output").style.fontWeight="bold"
+      // newDiv.setAttribute('style',"font-weight:bold;");
+      
+    }else{
+      document.getElementById("output").style.fontWeight=400
+
+      // newDiv.setAttribute('style',"font-weight:400;");
+    }
+    // document.getElementById("output").innerHTML = newDiv;
+    console.log(name);
   }
 
   var form = document.getElementById('myForm');
